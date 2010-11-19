931da6c5f60c6ce31862fe701deaf5c3d884d5cc
Simon COURTOIS <scourtois@linagora.com>
2010-11-19 17:35:08 +0100

    Adding some validation on User model

--- app.rb
+++ app.rb
@@ -47,8 +47,20 @@ end
 # User model
 #
 class User < ActiveRecord::Base
+  validates_uniqueness_of :name
+
+  before_update :remove_name
+
   def restricted?
     !self.is_restricted.zero?
+  end
+
+  private
+
+  def remove_name
+    if self.name_changed?
+      self.name = self.name_was
+    end
   end
 end
 
